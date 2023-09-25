e615cb61b9a530ce9d3959c414ccb419a51c88a5
Azguegou <axel.brun@etu.u-paris.fr>
2023-09-25 19:06:01 +0200

    initial-commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-import NeuralNetwork as neuralNetwork
 import ImageToByteArray as imageGetter
+import NeuralNetwork
 
 matrices = [
     np.random.rand(2, 3),
@@ -12,7 +12,7 @@ matrices = [
     np.random.rand(3, 1)
 ]
 
-weights, bias = neuralNetwork.generate_random_neural_network(1024, 5, 1500, 10)
+weights, bias = NeuralNetwork.generate_random_neural_network(1024, 5, 1500, 10)
 a, b = imageGetter.load_images()[0]
-lis = neuralNetwork.forward_propagation(b, weights, bias)
+lis = NeuralNetwork.forward_propagation(b, weights, bias)
 print(lis)
