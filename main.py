b28afebdc81b2440b7e84b9344ed06881abac139
BRUN Axel <axel.brun@iutseine.u-paris.domain>
2023-10-23 12:32:23 +0200

    fix_backpropagation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import NeuralNetwork as neuralNetwork
 import ImageToByteArray as imageGetter
 import trainer as trainer
+import JDD as jdd
 
 nb_inputs = 1024
 nb_layers = 3
@@ -10,7 +11,5 @@ nb_neurons = 100
 nb_outputs = 10
 
 weights, bias = neuralNetwork.generate_random_neural_network(nb_inputs, nb_layers, nb_neurons, nb_outputs)
-neuralNetwork.save(weights, bias, os.path.join(os.getcwd(), "data.txt"))
-a, b = imageGetter.load_images()[0]
-activations = neuralNetwork.forward_propagation(b, weights, bias)
-print(trainer.get_gradients(b, a, activations, weights))
+train_norm_X, train_norm_Y = jdd.training_set(40, 200)
+print(train_norm_X.shape)
